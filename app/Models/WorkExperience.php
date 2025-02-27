a34de6b1f958b5e93a202da90593806fecf06972
Mehedi Hasan Hasib <mehedi.mh50@gmail.com>
2025-02-28 00:06:04 +0600

    work experience model and migration added

---
+++ app/Models/WorkExperience.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class WorkExperience extends Model
+{
+    protected $table = 'work_experience';
+    protected $guarded = [];
+}
